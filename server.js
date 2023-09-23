78961045098e213d61f4670300ec31202407da4e
John Francis <john9francis@gmail.com>
2023-09-23 17:45:20 -0600

    now the server console logs when a client joins

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
 const port = 3000
 
+// set up ejs as view engine
 app.set('view engine', 'ejs')
+
+// serve the client-side code
 
 app.get('/', (req, res) => {
   res.render('index')
+  console.log('a client connected')
 })
 
-app.listen(port, () => {
+// socket.io stuff
+io.on('connection', (socket) => {
+  console.log('A user connected')
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected')
+  })
+})
+
+http.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
